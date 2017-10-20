440551a641df4211bdb5e243ea2368fc80785d35
Kennedy Opoku Asare <opokuasarekennedy@gmail.com>
2017-10-20 15:04:00 +0300

    document 2 named entity count added

--- Email Spam Classification.py
+++ Email Spam Classification.py
@@ -1,10 +1,11 @@
-#### Dependencies
+# Dependencies
 # 1. numpy
 # 2. scipy
 # 2. scikit-learn
 
 import os
 import numpy as np
+import nltk
 import re
 from collections import Counter
 from sklearn.model_selection import train_test_split
@@ -13,95 +14,133 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import LinearSVC
-from sklearn.feature_extraction.text import TfidfTransformer,TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import LabelBinarizer
-from sklearn.pipeline import Pipeline,FeatureUnion
-from sklearn.metrics import precision_score,recall_score,f1_score,average_precision_score
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import precision_score, recall_score, f1_score, average_precision_score
 from sklearn.base import BaseEstimator, TransformerMixin
 
 # #### Extracting F1 = number of URL, links in the message
 
 
-    
-
-
-
 # run this method onces, and then load the saved data and use subsequently
 # method saves dict_enron.npy, all_email_corpus
 def make_Dictionary(root_dir):
-    all_email_corpus={'text': [], 'class': []}
-   
-    emails_dirs = [os.path.join(root_dir,f) for f in os.listdir(root_dir)]    
-    all_words = []       
+    all_email_corpus = {'text': [], 'class': []}
+
+    emails_dirs = [os.path.join(root_dir, f) for f in os.listdir(root_dir)]
+    all_words = []
     for emails_dir in emails_dirs:
-        dirs = [os.path.join(emails_dir,f) for f in os.listdir(emails_dir)]
+        dirs = [os.path.join(emails_dir, f) for f in os.listdir(emails_dir)]
         for d in dirs:
-            emails = [os.path.join(d,f) for f in os.listdir(d)]
+            emails = [os.path.join(d, f) for f in os.listdir(d)]
             for mail in emails:
-                with open(mail) as m:                    
-                    email_words=[]
+                with open(mail) as m:
+                    email_words = []
                     for line in m:
-                        words = line.split()
+                        words = nltk.word_tokenize(line)  # line.split()
                         all_words += words
-                        email_words+=words
-                    emailClass='ham'
+                        email_words += words
+                    emailClass = 'ham'
                     print mail.split(".")[-2]
-                    if mail.split(".")[-2]=='spam':
-                        emailClass='spam'
+                    if mail.split(".")[-2] == 'spam':
+                        emailClass = 'spam'
                     all_email_corpus['text'].append(' '.join(email_words))
-                    all_email_corpus['class'].append(emailClass) #1 is spam , 0 is ham
-                        
+                    all_email_corpus['class'].append(
+                        emailClass)  # 1 is spam , 0 is ham
+
     dictionary = Counter(all_words)
     list_to_remove = dictionary.keys()
-    
+
     for item in list_to_remove:
-        if item.isalpha() == False: 
+        if item.isalpha() == False:
             del dictionary[item]
         elif len(item) == 1:
             del dictionary[item]
     dictionary = dictionary.most_common(3000)
-    vocabulary=sorted( [key for (key,value) in dictionary]) 
-    np.save('vocabulary.npy',vocabulary) 
-    np.save('all_email_corpus.npy',all_email_corpus)
-    
-    return vocabulary,all_email_corpus
+    vocabulary = sorted([key for (key, value) in dictionary])
+    np.save('vocabulary.npy', vocabulary)
+    np.save('all_email_corpus.npy', all_email_corpus)
 
-def evaluate_prediction(labels_test,predictions):
-    evaluationTable=[]
-    for key,value in predictions.iteritems():
-        
-        evaluation={}
-        evaluation['Classifier']=key
-        
+    return vocabulary, all_email_corpus
 
-        evaluation['Recall']=recall_score(labels_test,value)
-        evaluation['Precision']=precision_score(labels_test,value)
-        evaluation['F1 Score']=f1_score(labels_test,value)
-        evaluation['Average Precision score']=average_precision_score(labels_test,value)
-        
+
+def evaluate_prediction(labels_test, predictions):
+    evaluationTable = []
+    for key, value in predictions.iteritems():
+
+        evaluation = {}
+        evaluation['Classifier'] = key
+
+        evaluation['Recall'] = recall_score(labels_test, value)
+        evaluation['Precision'] = precision_score(labels_test, value)
+        evaluation['F1 Score'] = f1_score(labels_test, value)
+        evaluation['Average Precision score'] = average_precision_score(
+            labels_test, value)
+
         evaluationTable.append(evaluation)
     return evaluationTable
-class WordCountVectorizer(BaseEstimator,TransformerMixin):
+
+
+class WordCountVectorizer(BaseEstimator, TransformerMixin):
     """Takes a list of documents and extracts word counts in the document"""
+
     def __init__(self):
         pass
-    def count_words_doc(self,doc):
+
+    def count_words_doc(self, doc):
         """Returns the count of words in a document"""
-        all_words=doc.split()
-        all_words_iter=all_words
+        all_words = nltk.word_tokenize(doc)
+        all_words_iter = all_words
         for item in all_words_iter:
-            if item.strip().isalpha() == False: 
+            if item.strip().isalpha() == False:
                 all_words.remove(item)
-            
+
         return len(all_words)
-    def get_all_word_counts(self,docs):
-         """Encodes document to number of words, links"""
+
+    def get_all_word_counts(self, docs):
+         """Encodes document to number of words"""
          return [self.count_words_doc(d) for d in docs]
+
     def transform(self, docs, y=None):
         """The workhorse of this feature extractor"""
-        resultList=self.get_all_word_counts(docs)
+        resultList = self.get_all_word_counts(docs)
+        return np.transpose(np.matrix(resultList))
+
+    def fit(self, docs, y=None):
+        """Returns `self` unless something different happens in train and test"""
+        return self
+
+
+class NameEntityCountVectorizer(BaseEstimator, TransformerMixin):
+    """Takes a document and extracts count of named entities.
+        This class uses the NLTK parts of speach tagger
+    """
+
+    def __init__(self):
+        pass
+
+    def count_named_entities(self, doc):
+        """Returns a count of named entities in te a document"""
+        tokens = nltk.word_tokenize(doc)
+        pos = nltk.pos_tag(tokens)
+        namedEntityTags = ["NN", "NNP", "NNS", "NNPS"]
+        named_entities = []
+        for word, tag in pos:
+            if tag in namedEntityTags:
+                named_entities.append(word)
+        print named_entities
+        return len(namedEntities)
+
+    def get_all_named_entities(self, doc):
+        """Encodes document to number of named entities"""
+        return [self.count_named_entities(d) for d in docs]
+
+    def transform(self, docs, y=None):
+        """The workhorse of this feature extractor"""
+        resultList=self.get_all_named_entities(docs)
         return np.transpose(np.matrix(resultList))
 
     def fit(self, docs, y=None):
@@ -148,11 +187,11 @@ class URLCountVectorizer(BaseEstimator,TransformerMixin):
         """Returns `self` unless something different happens in train and test"""
         return self
 
-#### Step 0. extracting email corpus and vocabulary
-#root_dir = 'dataset'
-#make_Dictionary(root_dir)
+# Step 0. extracting email corpus and vocabulary
+# root_dir = 'dataset'
+# make_Dictionary(root_dir)
 
-#### step 0.1. 
+# step 0.1. 
 all_email_corpus=np.load("all_email_corpus.npy").item()
 vocabularyList=np.load("vocabulary.npy").tolist()
 documents=all_email_corpus['text']
@@ -161,11 +200,11 @@ labels=all_email_corpus['class']
 binarizer=LabelBinarizer()
 binarisedLables=binarizer.fit_transform(labels).ravel()
 
-#x is document
+# x is document
 # y is the labels or classes
 document_train, document_test, labels_train, labels_test = train_test_split(documents, binarisedLables, test_size=0.40)
 
-### step 1. Only term frequency feature
+# step 1. Only term frequency feature
 print "With TF"
 word2vectTransformer=CountVectorizer(vocabulary=vocabularyList)
 
@@ -193,14 +232,14 @@ predictions["SVM"]=SVM_pipeline.fit(document_train,labels_train).predict(documen
 predictions['Naive Bayesian']=NB_pipeline.fit(document_train,labels_train).predict(document_test)
 predictions['Random Forest']=RF_pipeline.fit(document_train,labels_train).predict(document_test)
 predictions['K Nearest Neighbour']=KNN_pipeline.fit(document_train,labels_train).predict(document_test)
-#print type(predictions)
+# print type(predictions)
 
 scores=evaluate_prediction(labels_test,predictions)
 
 
 print scores
 
-#### Step 2.  TF.IDF
+# Step 2.  TF.IDF
 print "With TF.IDF"
 documents2TfidfVector =TfidfVectorizer(vocabulary=vocabularyList)
 SVM_pipeline=Pipeline([
@@ -230,7 +269,7 @@ scores=evaluate_prediction(labels_test,predictions)
 
 print scores
 
-#### Step 3. Feature component set F1...F5
+# Step 3. Feature component set F1...F5
 
 print "using feature set F1.F3"
 
